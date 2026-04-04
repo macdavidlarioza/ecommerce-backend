3240e38ad3a0201238a9c54c34aeef7c85e903a9
Mac David G. Larioza <mglarioza@SEALINK.LOCAL>
2026-04-04 10:04:18 +0800

    fix: update CORS for production

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
   app.enableCors({
-    origin: 'http://localhost:5173',
-  });
+  origin: [
+    'http://localhost:5173',                          // local dev
+    'https://e-commerce-app-three-smoky.vercel.app',  // your Vercel URL
+  ],
+  credentials: true,
+});
 
   await app.listen(process.env.PORT ?? 3000);
 }
