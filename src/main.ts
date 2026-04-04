765a6b0416ae94e51589431e9a6248542a66b2c5
Mac David G. Larioza <mglarioza@SEALINK.LOCAL>
2026-04-04 10:39:21 +0800

    main.ts update 2

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
   origin: [
     'http://localhost:5173',                          // local dev
-    'macbid.vercel.app',  // your Vercel URL
+    'https://macbid.vercel.app',  // your Vercel URL
   ],
   credentials: true,
 });
